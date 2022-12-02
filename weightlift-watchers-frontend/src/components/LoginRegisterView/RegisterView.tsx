ae689eca7d20de2af335a16713c106670a07c877
John <john.pcooke94@gmail.com>
2022-12-01 22:23:22 -0500

    Navigate back to login page once user is registered

--- weightlift-watchers-frontend/src/components/LoginRegisterView/RegisterView.tsx
+++ weightlift-watchers-frontend/src/components/LoginRegisterView/RegisterView.tsx
@@ -1,9 +1,12 @@
 import {Button, Container, Form, FormControl, FormGroup, FormLabel, Row} from 'react-bootstrap';
-import {FormEvent} from 'react';
+import {FormEvent, useState} from 'react';
 import {createUserRequest} from '../../api/types/createUserRequest';
 import {createNewUser} from '../../api';
+import {Link, Navigate} from 'react-router-dom';
 
 const RegisterView = () => {
+
+    const [registered, setRegistered] = useState<boolean>(false);
 
     const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
@@ -17,49 +20,65 @@ const RegisterView = () => {
         // TODO: replace these alerts with something better
         if (passwordInput !== confirmPasswordInput) {
             alert("Your password does not match your password confirmation, please check and try again.");
-        } else {
-            const request: createUserRequest = {
-                username: usernameInput as string,
-                password: passwordInput as string
-            }
+            return;
+        }
 
-            try {
-                const response = await createNewUser(request);
-                if (response.status === 200) {
-                    alert("Success!");
-                }
-            } catch (err) {
-                alert('An error was encountered while registering your account');
-            }
+        if (!usernameInput) {
+            alert('You must input a username to identify your account');
+            return;
+        }
 
+        const request: createUserRequest = {
+            username: usernameInput as string,
+            password: passwordInput as string
+        }
+
+        try {
+            const response = await createNewUser(request);
+            if (response.status === 200) {
+                setRegistered(true);
+                alert("Success! You may now log in.");
+            }
+        } catch (err) {
+            alert('An error was encountered while registering your account');
         }
 
     }
 
-  return (
-      <Container>
-          <Row style={{textAlign: 'center'}}>
-              <h1>Register</h1>
-          </Row>
-          <Row>
-              <Form onSubmit={handleSubmit}>
-                  <FormGroup>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl placeholder={'Enter your username'} name={'username'}/>
-                  </FormGroup>
-                  <FormGroup>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl type={'password'} placeholder={'Enter your password'} name={'password'}/>
-                  </FormGroup>
-                  <FormGroup>
-                      <FormLabel>Confirm Password</FormLabel>
-                      <FormControl type={'password'} placeholder={'Confirm your password'} name={'confirmPassword'}/>
-                  </FormGroup>
-                  <Button style={{marginTop: '20px'}} type={'submit'}>Submit</Button>
-              </Form>
-          </Row>
-      </Container>
-  );
-}
+    if (registered) {
+        return <Navigate to={'/login'}/>
+    }
+
+    return (
+        <Container>
+            <Row style={{textAlign: 'center'}}>
+                <h1>Register</h1>
+            </Row>
+            <Row>
+                <Form onSubmit={handleSubmit}>
+                    <FormGroup>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl placeholder={'Enter your username'} name={'username'}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl type={'password'} placeholder={'Enter your password'} name={'password'}/>
+                    </FormGroup>
+                    <FormGroup>
+                        <FormLabel>Confirm Password</FormLabel>
+                        <FormControl type={'password'} placeholder={'Confirm your password'} name={'confirmPassword'}/>
+                    </FormGroup>
+                    <Button style={{marginTop: '20px'}} type={'submit'}>Submit</Button>
+                </Form>
+            </Row>
+            <Row style={{marginTop: '20px'}}>
+                <Container>
+                    <Link to={'/login'}>Back to log in</Link>
+                </Container>
+            </Row>
+        </Container>
+    );
+
+};
 
 export default RegisterView;
