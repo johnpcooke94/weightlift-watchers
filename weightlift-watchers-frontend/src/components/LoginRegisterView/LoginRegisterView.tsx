8524390dd82bbdb83bed7f96c8e8efc5410229b1
John <john.pcooke94@gmail.com>
2022-12-01 20:17:43 -0500

    Basic implementation of the login view

--- weightlift-watchers-frontend/src/components/LoginRegisterView/LoginRegisterView.tsx
+++ weightlift-watchers-frontend/src/components/LoginRegisterView/LoginRegisterView.tsx
@@ -1,7 +1,32 @@
+import {Button, Container, Form, FormControl, FormGroup, FormLabel, Row} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 const LoginRegisterView = () => {
     return (
-        <p>Login or register</p>
+        <Container>
+            <Row style={{textAlign: 'center'}}>
+                <h1>Log in</h1>
+            </Row>
+            <Row>
+                <Form>
+                    <FormGroup controlId={'username'}>
+                        <FormLabel>Username</FormLabel>
+                        <FormControl placeholder={'Enter your username'}></FormControl>
+                    </FormGroup>
+                    <FormGroup controlId={'password'}>
+                        <FormLabel>Password</FormLabel>
+                        <FormControl placeholder={'Enter your password'}></FormControl>
+                    </FormGroup>
+                    <Button style={{marginTop: '20px'}} type={'submit'}>Submit</Button>
+                </Form>
+            </Row>
+            <Row style={{marginTop: '20px'}}>
+                <Container>
+                    <div>Don't have an account?</div>
+                    <Link to={'/register'}>Register here</Link>
+                </Container>
+            </Row>
+        </Container>
     );
 }
 
